e7a3a5893dc0988796449bb75ba39d1d76ce42c9
Fernando Minoda <minoda@gmail.com>
2018-08-01 10:59:28 -0300

    Update jenkins-pipeline.groovy

--- jenkins-pipeline.groovy
+++ jenkins-pipeline.groovy
@@ -1,14 +1,14 @@
  node('maven') {
     stage('build') {
         echo 'building app :)'
-        openshiftBuild(buildConfig: 'workshop', showBuildLogs: 'true')
+        openshiftBuild(buildConfig: 'workshp', showBuildLogs: 'true')
     }
     stage('verify') {
         echo 'dummy verification....'
     }
     stage('deploy') {
         input 'Manual Approval'
-        openshiftDeploy(deploymentConfig: 'workshop')
+        openshiftDeploy(deploymentConfig: 'workshp')
     }
     stage('promoting to QA') {
        echo 'fake stage...'
